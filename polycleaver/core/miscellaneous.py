562b08ebcde2bfd17feb87d40156f4dd20cb0c4e
Eric Mates-Torres <ericmates@gmail.com>
2023-03-30 12:51:39 +0200

    Re-built function distribution in external modules for future generation of mx functionality.

---
+++ polycleaver/core/miscellaneous.py
@@ -0,0 +1,6 @@
+def load_bar(index, total):
+    percent = 100.*(index+1)/total
+    sys.stdout.write('\r')
+    sys.stdout.write("Sanitizing slabs: [{:{}}] {:>3}%"
+                        .format('='*int(percent/(100.0/30)),
+                                30, int(percent)))
