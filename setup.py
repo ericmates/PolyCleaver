eb3ef3207b8067ea9ae5e4c9f811af4424e8e76c
Eric Mates-Torres <ericmates@gmail.com>
2023-05-08 14:52:39 +0200

    Minor fix.

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
 VERSION = '0.0.1'
 DESCRIPTION = 'Package for the generation of non-polar and stoichiometric surfaces from ionic structures.'
 LONG_DESCRIPTION = 'Package for the generation of non-polar and stoichiometric surfaces from ionic structures.'
@@ -19,7 +14,6 @@ setup(
     author_email="<eric.mates@uab.cat>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=['pymatgen'],
     keywords=['python', 'slab', 'surface', 'miller', 'ionic', 'mineral'],
